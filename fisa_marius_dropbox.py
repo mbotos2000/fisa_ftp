4c06bcd5b2e4a4e8f7232c7207ce2018c23a6b96
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2024-12-21 17:47:22 +0200

    Update fisa_marius_dropbox.py

--- fisa_marius_dropbox.py
+++ fisa_marius_dropbox.py
@@ -632,12 +632,11 @@ if st.session_state['file']!=None or st.session_state['ut']:
         data1['ore_s']=14*data1['numarore'].astype(int)
         st.write(doc_result.body[5][4][-1][0])
         try:
-	    add_selectbox_D = st.selectbox(
-                'Disciplina?',
+         add_selectbox_D = st.selectbox('Disciplina?',
                 nume_di,key='M_2_1',index=find_closest_match_index(doc_result.body[3][0][3][0], nume_di),
                 help='Toate datele asociate disciplinei vor fi inserate in fisa disciplinei automat din planul de invatamant')
         except:
-	    add_selectbox_D = st.selectbox(
+         add_selectbox_D = st.selectbox(
                 'Disciplina?',
                 nume_di,key='M_2_1',
                 help='Toate datele asociate disciplinei vor fi inserate in fisa disciplinei automat din planul de invatamant')
