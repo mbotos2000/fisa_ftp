db74dd74a54ed5829fe19824f1977fcfe20317d5
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2025-02-26 11:57:05 +0200

    Update fisa_marius_dropbox.py

--- fisa_marius_dropbox.py
+++ fisa_marius_dropbox.py
@@ -556,11 +556,11 @@ if not(st.session_state['ut']):
 if st.session_state['file']!=None or st.session_state['ut']:
   if st.session_state['file']!=None: 
     doc_result = docx2python(st.session_state['file'])
-    doc1 = docx2python(st.session_state['file'])
+    doc = docx2python(st.session_state['file'])
     
     table_positions = {}  # Store data in {table_index: {row_index: {col_index: value}}}
 
-    for table_index, table in enumerate(doc1.tables):
+    for table_index, table in enumerate(doc.tables):
         table_positions[table_index] = {}
         
         for row_index, row in enumerate(table.rows):
