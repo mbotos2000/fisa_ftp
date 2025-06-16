017b5cef6fd5ab1bcaeee165ed3fd7df67872cf4
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2025-06-16 15:06:53 +0300

    Update fisa_marius_dropbox.py

--- fisa_marius_dropbox.py
+++ fisa_marius_dropbox.py
@@ -1337,7 +1337,7 @@ if st.session_state['file']!=None or st.session_state['ut']:
          redirect_url1 = "https://fisaconstructiiutcn.streamlit.app/"
          st.markdown(f"[Incaraca alt fisier docx]({redirect_url1})")
          st.stop()  # Only if needed, or use JS for redirect
-        if st.button("Cauta fisa noua in baza de date")
+        if st.button("Cauta fisa noua in baza de date"):
          st.write("Acceseaza linkul de mai jos pentru a cauta din nou in baza de date o fisa")
          redirect_url2 = "https://fisaconstructiibaza.streamlit.app/"
          st.markdown(f"[Cauta in baza de date]({redirect_url2})")
