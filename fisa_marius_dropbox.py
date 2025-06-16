9fad293c792e1c2cb764d9142d2a293d776da48a
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2025-06-16 15:08:45 +0300

    Update fisa_marius_dropbox.py

--- fisa_marius_dropbox.py
+++ fisa_marius_dropbox.py
@@ -1332,12 +1332,14 @@ if st.session_state['file']!=None or st.session_state['ut']:
         ftp_server1.cwd('./public_html/Fise/2025')
         ftp_server1.storbinary(f'STOR {file_name}', docx_buff)
         ftp_server1.quit()
-        if st.button("Incarca alt fisier docx"):
+        sub1=st.button("Incarca alt fisier docx")
+        if sub1:
          st.write("Acceseaza linkul de mai jos pentru a incarca o alta fisa in format docx")
          redirect_url1 = "https://fisaconstructiiutcn.streamlit.app/"
          st.markdown(f"[Incaraca alt fisier docx]({redirect_url1})")
          st.stop()  # Only if needed, or use JS for redirect
-        if st.button("Cauta fisa noua in baza de date"):
+        sub2=st.button("Cauta fisa noua in baza de date")
+	if sub2:
          st.write("Acceseaza linkul de mai jos pentru a cauta din nou in baza de date o fisa")
          redirect_url2 = "https://fisaconstructiibaza.streamlit.app/"
          st.markdown(f"[Cauta in baza de date]({redirect_url2})")
